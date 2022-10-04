a3ed7440992106203ae71b81c90e8ff48bae0bab
Eleazar Flores <eleazar.flores@bancoazteca.com>
2022-10-04 12:15:50 -0500

    commit initial V2

---
+++ tableViewEFC/tableViewEFC/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  tableViewEFC
+//
+//  Created by efloresco on 04/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
